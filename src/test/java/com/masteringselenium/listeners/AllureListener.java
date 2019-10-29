06b50a1dc91b55dbfcb218881f9c0355b1256ec0
chudilka1 <yepishevsanya@gmail.com>
2019-10-29 19:01:12 +0200

    Update allure listener

--- src/test/java/com/masteringselenium/listeners/AllureListener.java
+++ src/test/java/com/masteringselenium/listeners/AllureListener.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
-import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
 
 import com.masteringselenium.DriverFactory;
 
@@ -13,24 +13,24 @@ import io.qameta.allure.Attachment;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AllureListener implements ITestListener {
+public class AllureListener extends TestListenerAdapter {
 
 	private static String getTestMethodName(ITestResult iTestResult) {
 		return iTestResult.getMethod().getConstructorOrMethod().getName();
 	}
 
 	//Text attachment for Allure
-	@Attachment(value = "Page screenshot", type = "image/png")
-	public byte[] saveScreenshotPNG(WebDriver driver) {
+	@Attachment(value = "Screenshot", type = "image/png")
+	private byte[] saveScreenshotPNG(WebDriver driver) {
 		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 	}
 
-	@Attachment(value = "{0}", type = "text/plain")
-	public static String saveTextLog(String message) {
+	@Attachment(value = "{message}", type = "text/plain")
+	private static String saveTextLog(String message) {
 		return message;
 	}
 
-	@Attachment(value = "{0}", type = "text/html")
+	@Attachment(value = "{html}", type = "text/html")
 	public static String attachHtml(String html) {
 		return html;
 	}
@@ -61,12 +61,13 @@ public class AllureListener implements ITestListener {
 		log.warn("Test failed: " + getTestMethodName(iTestResult) + " failed");
 		Object testClass = iTestResult.getInstance();
 		WebDriver driver = DriverFactory.getDriver();
-		if(driver != null) {
+		if (driver != null) {
 			log.warn("Screenshot captured for test case: " + testClass + getTestMethodName(iTestResult));
 			saveScreenshotPNG(driver);
 		}
 		//Save log
 		saveTextLog(getTestMethodName(iTestResult) + "failed and screenshot taken!");
+		attachHtml(getTestMethodName(iTestResult) + "atached html logs");
 	}
 
 	@Override
